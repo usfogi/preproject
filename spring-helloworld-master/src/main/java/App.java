84beaf8e3f4340380b87ad1360002b15fb3b9e6a
Michael Egorov <usualgold@gmail.com>
2020-06-24 19:22:11 +0700

    212 создать Bean Cat с аннотацией Scope создания каждый раз новый бин

---
+++ spring-helloworld-master/src/main/java/App.java
@@ -0,0 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean.equals(bean1));
+        System.out.println(cat.equals(cat1));
+    }
+}
