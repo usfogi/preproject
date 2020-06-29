b69d5cd0a6038fd0a2d4d59d911900db18b7b3ce
Michael Egorov <usualgold@gmail.com>
2020-06-29 17:57:51 +0700

    214 сказ про кощея и три способа Autowired

---
+++ spring-types-of-wiring/src/main/java/koschei/KoscheiTheDeathless.java
@@ -0,0 +1,21 @@
+package koschei;
+
+import koschei.models.Ocean1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KoscheiTheDeathless {
+
+    private Ocean1 ocean;
+
+    @Autowired
+    public void setOcean(Ocean1 ocean) {
+        this.ocean = ocean;
+    }
+
+    public String getRulesByDeth() {
+        return "На свете есть океан , " + ocean.toString();
+    }
+
+}
